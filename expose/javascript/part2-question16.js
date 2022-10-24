81620ee98a564184e53e89659544999d9cad5355
Nikhil Dange <nsdange@ucsd.edu>
2022-10-23 20:23:42 -0700

    fixed part2-q16

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -12,14 +12,3 @@ for (const property in statistics) {
         console.log(property);
     }
 }
-
-
-
-function printNums() {
-    console.log(1);
-    setTimeout(function() {console.log(2);}, 1000);
-    setTimeout(function() { console.log(3);}, 0);
-    console.log(4);
-}
-
-console.log(printNums());
